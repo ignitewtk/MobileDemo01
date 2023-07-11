96c6c7341317cf14056149bef9629d59a4a08089
Tony.Wu <uq44961879@gmail.com>
2023-07-12 00:01:10 +1000

    <dev>: handle HTTP request from azure server.

--- Components/SessionList.js
+++ Components/SessionList.js
@@ -1,9 +1,10 @@
-import { Alert, StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
+import { Alert, StyleSheet, Text, TextInput, View, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react'
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 
-function SessionItem(props) {
-
+function SessionItem(props) { 
+    const [username, setUsername] = useState('');
     _delete = () => {
         Alert.alert("Do you want to delete this?")
     }
@@ -22,17 +23,34 @@ for (let i=0; i < 5; i++) {
     items.push("User"+i)
 }
 
-export default function SessionList() {
-  return (
-    <ScrollView style={styles.container}>
-        {
-            items.map((item, index) => (
-                <SessionItem key={index} username={item}/>
-            ))
-        }
-    </ScrollView>
-  );
+const SessionList = ({navigation, route}) => {
+
+    _search = () => {
+        Alert.alert("searching...")
+    }
+
+    return (
+        <View>
+            <Text> Welcome, {route.params.name} !</Text>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={_search}>
+                    <Ionicons name="search" size={30} color="#d4d9d6" margin={5} padding={10}/>
+                </TouchableOpacity>
+                <TextInput style={styles.textInput}/>
+            </View>
+            <ScrollView style={styles.container}>
+                {
+                    items.map((item, index) => (
+                        <SessionItem key={index} username={item}/>
+                        ))
+                }
+            </ScrollView>
+        </View>
+        
+    );
 }
+
+export default SessionList;
 
 const styles = StyleSheet.create({
     container: {
@@ -46,13 +64,24 @@ const styles = StyleSheet.create({
         height: 60,
         backgroundColor: "#d4d9d6"
     },
-
     icon: {
         margin: 10,
         padding: 10,
         height: 60,
         width: 60,
-        backgroundColor: "#c7c9c8"
-        
+        backgroundColor: "#c7c9c8"  
     },
+    textInput: {
+        margin: 10,
+        width: 250,
+        height: 40,
+        borderWidth: 1, 
+        borderColor: "#d4d9d6",
+    },
+    searchIcon: {
+        margin: 10,
+        width: 40,
+        height: 40,
+    },
+
 })
--- Views/Authentication.js
+++ Views/Authentication.js
@@ -1,22 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Alert, View, TextInput, Button, Image } from 'react-native';
 
 import React, { useState } from 'react'
 
 export default function Authentication({navigation}) {
-  
-  const [username, setUsername] = useState('');
-  
 
-
+  const [username, setUsername] = useState("")
+  const _usernameChange = (newText) => {
+    console.log(newText)
+    setUsername(newText)
+  }
+  const _onSubmit = (params) => {
+    console.log('customer1')
+    fetch('http://backend202307112242.azurewebsites.net/webapp/page2', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify({
+      //   userName: 'customer1',
+      //   password: 'customer1'
+      // })
+    })
+    .then(response => response.json()).then((json) => {
+      console.log(json.message)
+    })
+  }
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require('../assets/favicon.png')} />
-      <TextInput style={styles.textInput} placeholder='Username'/>
-
+      <TextInput style={styles.textInput} placeholder='Username'
+        onChangeText={newText => setUsername(newText)} 
+        />
       <TextInput style={styles.textInput} placeholder='Password'/>
-      <Button title="Submit" 
-        onPress={() => navigation.navigate('SessionList', {name: 'Admin'})} />
+      <Button title="Next" 
+        onPress={() => navigation.navigate('SessionList', {name: username})} />
+      <Button title="Sign In" 
+        onPress={_onSubmit} />
       <StatusBar style="auto" />
     </View>
   );
